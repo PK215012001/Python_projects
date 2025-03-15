8c3ff40c9976ff88f46ac1a8c861c2ee7f7ad3ea
Pavan Kalyan <gpavankalyanreddy0@gmail.com>
2025-03-15 20:35:29 +0530

    Took variable lengths for each password and handled file errors better

--- random_password_generator.py
+++ random_password_generator.py
@@ -33,28 +33,38 @@ def generate_random_password(length):
     return ''.join(password)
 
 
-def password_list(count, length):
+def password_list(count):
     ''''Function to generate a list of given count of passwords'''
     for i in range(count):
+        length = random.choice(range(8, 17))
         password = generate_random_password(length)
         yield password
 
 
 def file_write(passwords):
     '''Function to write the genrated password into a file'''
-    with open(FILE, 'w') as f:
-        for password in passwords:
-            f.write(f'{password}\n')
+    try:
+        with open(FILE, 'w') as f:
+            for password in passwords:
+                f.write(f'{password}\n')
+        print('Passwords written file.')
+    except IOError as e:
+        print(f'Error writting to a file:{e}')
 
+
+# def radnom_password_length():
+#     '''Function to randomly choose a number between 8 and 16'''
+#     length = random.choice(range(8, 17))
+#     return length
 
 
 def main():
-    passwrd_lenght = get_number('Enter the length of the password you want: ')
-    while(passwrd_lenght < 8 or passwrd_lenght > 16):
-        print('Password length should be in between 8 to 16 characters')
-        passwrd_lenght = get_number('Enter the length of the password you want: ')
+    # passwrd_lenght = radnom_password_length()
+    # while(passwrd_lenght < 8 or passwrd_lenght > 16):
+    #     print('Password length should be in between 8 to 16 characters')
+    #     passwrd_lenght = get_number('Enter the length of the password you want: ')
     passwrd_cnt = get_number('Enter the random passwords count: ')
-    list_passwords = password_list(passwrd_cnt, passwrd_lenght)
+    list_passwords = password_list(passwrd_cnt)
     file_write(list_passwords)
 
 
